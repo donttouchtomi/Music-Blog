01bf0244a97d84950ef1ddb809bac1519beaba74
Tomisin Akinyemi <tomisinakin6@gmail.com>
2024-10-26 10:43:30 +0100

    First commit

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const secondHeader = document.querySelector(".navBar");
 
 document.getElementById("search").onclick = () => {
   searchForm.classList.toggle("active");
+  secondHeader.classList.remove("active");
 };
 
 document.getElementById("menu").onclick = () => {
   secondHeader.classList.toggle("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  secondHeader.classList.remove("active");
 };
